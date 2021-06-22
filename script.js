87badb2c35fb7d8485de8999ad6ac6cc58a89b3c
Whitely <matthewwhitely77@gmail.com>
2021-06-22 18:53:41 -0400

    animations

--- script.js
+++ script.js
@@ -9,26 +9,46 @@ app.profile = document.querySelector(".profile");
 app.portfolio = document.querySelector(".portfolio");
 app.contact = document.querySelector(".contact");
 
-//animations
+//animations FOR PROFILE AND HEADER
 app.headerAnimation = document.querySelector(".animationContainer");
 app.tractor = document.querySelector(".tractor");
 app.tow = document.querySelector(".tow");
-app.absoultePosition = document.que;
+app.profileAnimation = document.querySelector(".profileAnimation2");
+
+//ANIMATION FOR PORTFOLIO
+app.portfolioAnimation = document.querySelector(".portfolioAnimation2");
 
 //button functions
 app.buttonActions = () => {
   app.profileBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    app.profile.classList.toggle("hidden");
+    app.profile.classList.remove("hidden");
+    app.profileAnimation.classList.toggle("profileAnimation2");
+    app.profileAnimation.classList.toggle("profileAnimation");
     app.portfolio.classList.add("hidden");
     app.contact.classList.add("hidden");
+    app.tractor.remove();
+    app.tow.remove();
+
+    //for portfolio so it will restart from beggining
+    app.portfolioAnimation.classList.remove("portfolioAnimation");
+    app.portfolioAnimation.classList.add("portfolioAnimation2");
   });
 
   app.portfolioBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    app.portfolio.classList.toggle("hidden");
+    //portfolio animation
+    app.portfolioAnimation.classList.toggle("portfolioAnimation");
+    app.portfolioAnimation.classList.toggle("portfolioAnimation2");
+    app.portfolio.classList.remove("hidden");
+    //making sure the other sections are hidden
     app.profile.classList.add("hidden");
     app.contact.classList.add("hidden");
+    //profile animation so when you click it it will start for the beggining
+    app.profileAnimation.classList.remove("profileAnimation");
+    app.profileAnimation.classList.add("profileAnimation2");
+    app.tractor.remove();
+    app.tow.remove();
   });
 
   app.contactBtn.addEventListener("click", function (e) {
@@ -36,6 +56,13 @@ app.buttonActions = () => {
     app.contact.classList.toggle("hidden");
     app.profile.classList.add("hidden");
     app.portfolio.classList.add("hidden");
+
+    //restart animation form other sections
+    app.profileAnimation.classList.remove("profileAnimation");
+    app.profileAnimation.classList.add("profileAnimation2");
+
+    app.portfolioAnimation.classList.remove("portfolioAnimation");
+    app.portfolioAnimation.classList.add("portfolioAnimation2");
   });
 };
 
